771a6a9a97c7e80a59c7aec0c290c4a4c1a53650
truongthotelecom-bit <truongthotelecom@gmail.com>
2026-08-11 20:11:26 +0700

    UI: Always show RangePicker in TransactionHistory filter bar

--- client/src/pages/TransactionHistory.jsx
+++ client/src/pages/TransactionHistory.jsx
@@ -364,15 +364,13 @@ export default function TransactionHistory() {
           </Select>
 
           {/* Vị trí 5: Từ ngày đến ngày */}
-          {filters.dateRangeType === DATE_RANGES.CUSTOM && (
-            <RangePicker
-              className="w-[260px]"
-              placeholder={['Từ ngày', 'Đến ngày']}
-              format="DD/MM/YYYY"
-              value={filters.customDateRange}
-              onChange={dates => setFilters({ ...filters, dateRangeType: DATE_RANGES.CUSTOM, customDateRange: dates })}
-            />
-          )}
+          <RangePicker
+            className="w-[260px]"
+            placeholder={['Từ ngày', 'Đến ngày']}
+            format="DD/MM/YYYY"
+            value={filters.customDateRange}
+            onChange={dates => setFilters({ ...filters, dateRangeType: DATE_RANGES.CUSTOM, customDateRange: dates })}
+          />
 
           {/* Vị trí 6: Trạng thái GD */}
           <Select
